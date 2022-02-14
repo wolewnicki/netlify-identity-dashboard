219f44c5ed7479b278fad39999cded5acab63ef5
wolewnicki <wolewnicki@yahoo.com>
2022-02-14 15:56:30 -0500

    trying to find the context

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 netlifyIdentity.on('init', user => {
   console.log(user)
 })
+
+if (netlifyIdentity.currentUser() === null) {
+  window.location.replace('https://admiring-poitras-f07eb5.netlify.app/')
+}
+
+console.log(netlifyIdentity.currentUser());
