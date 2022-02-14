c79baba38f0bcc37e7fda5e59041ec696c3e463e
wolewnicki <wolewnicki@yahoo.com>
2022-02-14 16:04:57 -0500

    trying to find the context

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,5 @@
-if (netlifyIdentity.currentUser() === null) {
-  window.location.replace('https://admiring-poitras-f07eb5.netlify.app/')
-}
+// if (netlifyIdentity.currentUser() === null) {
+//   window.location.replace('https://admiring-poitras-f07eb5.netlify.app/')
+// }
+
+console.log(netlifyIdentity.currentUser());
