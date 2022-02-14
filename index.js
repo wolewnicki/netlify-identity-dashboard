e9c2618793a2a963051b4d50c2cf4a645b13cce5
wolewnicki <wolewnicki@yahoo.com>
2022-02-14 16:18:13 -0500

    trying to find the context

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const redirect = () => {
   }
 }
 
-netlifyIdentity.currentUser();
-netlifyIdentity.on('login', () => redirect);
+console.log(netlifyIdentity.currentUser());
+netlifyIdentity.on('init', () => redirect);
 
 if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
   window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
