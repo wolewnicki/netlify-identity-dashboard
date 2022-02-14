73fb1d070659e6ac5bf6603e391e2d1ff4135668
wolewnicki <wolewnicki@yahoo.com>
2022-02-14 16:10:00 -0500

    trying to find the context

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const redirect = () => {
 
 netlifyIdentity.on('login', redirect);
 
-if (window.location !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
+if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
   window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
 }
