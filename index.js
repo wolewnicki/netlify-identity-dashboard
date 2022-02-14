522a47302fc31dad8bafb8da752c06ec0bec92cb
wolewnicki <wolewnicki@yahoo.com>
2022-02-14 15:51:35 -0500

    trying to find the context

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const handler = async (evt, ctx) => {
-  console.log(evt);
-  console.log(ctx)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({message: "Hello world!"})
-  }
-}
-
-handler();
+console.log(netlifyIdentity);
