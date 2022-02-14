df4737d6b03c7631d7bd2289e41009146f15544c
wolewnicki <wolewnicki@yahoo.com>
2022-02-14 16:14:10 -0500

    trying to find the context

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const redirect = () => {
+  if(window.location.href === 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() !== null) {
+    window.location.replace('https://admiring-poitras-f07eb5.netlify.app/dashboard.html')
+  }
+}
 
-netlifyIdentity.on('login', () => window.location.replace('https://admiring-poitras-f07eb5.netlify.app/dashboard.html'));
+netlifyIdentity.on('login', () => redirect);
 
 if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
   window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
