200933533068b26ce738a0c77646f34702d7d066
wolewnicki <wolewnicki@yahoo.com>
2022-02-15 15:05:42 -0500

    pls work

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-System.register(["netlify-identity-widget"], function (exports_1, context_1) {
-    "use strict";
-    var netlifyIdentity, redirect, logout;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [
-            function (netlifyIdentity_1) {
-                netlifyIdentity = netlifyIdentity_1;
-            }
-        ],
-        execute: function () {
-            netlifyIdentity.init({ container: '.netlify-container' });
-            redirect = function () {
-                if (window.location.href === 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() !== null) {
-                    window.location.replace('https://admiring-poitras-f07eb5.netlify.app/dashboard.html');
-                }
-            };
-            exports_1("logout", logout = function () {
-                if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
-                    window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
-                }
-            });
-            netlifyIdentity.on('login', function (u) {
-                redirect();
-            });
-            if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
-                window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
-            }
-            netlifyIdentity.on('logout', logout);
-        }
-    };
+import * as netlifyIdentity from 'netlify-identity-widget';
+netlifyIdentity.init({ container: '.netlify-container' });
+const redirect = () => {
+    if (window.location.href === 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() !== null) {
+        window.location.replace('https://admiring-poitras-f07eb5.netlify.app/dashboard.html');
+    }
+};
+const logout = () => {
+    if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
+        window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
+    }
+};
+netlifyIdentity.on('login', (u) => {
+    redirect();
 });
+if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
+    window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
+}
+netlifyIdentity.on('logout', logout);
--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const redirect = (): void => {
   }
 }
 
-export const logout = (): void => {
+const logout = (): void => {
     if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && netlifyIdentity.currentUser() === null) {
     window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
   }
