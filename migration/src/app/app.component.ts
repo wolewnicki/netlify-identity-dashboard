2aa87aedc06ec94265ff2e7542f811045cceb1cd
wolewnicki <wolewnicki@yahoo.com>
2022-02-19 19:40:24 -0500

    rejected support of safari 15.2-15.3

--- migration/src/app/app.component.ts
+++ migration/src/app/app.component.ts
@@ -8,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   netlifyIdentity = (window as any).netlifyIdentity;
 
-  ngOnInit(): void {
-    console.log(this.netlifyIdentity)
-    console.log(this.netlifyIdentity.currentUser())
-    this.netlifyIdentity.init();
+  redirect = () => {
+    if(window.location.href === 'https://admiring-poitras-f07eb5.netlify.app/' && this.netlifyIdentity.currentUser() !== null) {
+      window.location.replace('https://admiring-poitras-f07eb5.netlify.app/dashboard')
+    }
   }
+
+  logout = () => {
+      if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && this.netlifyIdentity.currentUser() === null) {
+      window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
+    }
+  }
+
+  ngOnInit(): void {
+    this.netlifyIdentity.init();
+
+    this.netlifyIdentity.on('logout', this.logout)
+
+    this.netlifyIdentity.on('login', () => {
+      this.redirect()
+    });
+
+    if (window.location.href !== 'https://admiring-poitras-f07eb5.netlify.app/' && this.netlifyIdentity.currentUser() === null) {
+      window.location.replace('https://admiring-poitras-f07eb5.netlify.app/');
+    } 
+  }
+ 
 }
