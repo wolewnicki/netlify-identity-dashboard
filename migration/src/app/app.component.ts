12c5639b0d89e5b7de9747ed7ea8e6ae440c84f5
wolewnicki <wolewnicki@yahoo.com>
2022-02-19 19:25:17 -0500

    rejected support of safari 15.2-15.3

--- migration/src/app/app.component.ts
+++ migration/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'migration';
+export class AppComponent implements OnInit {
+  netlifyIdentity = (window as any).netlifyIdentity;
+
+  ngOnInit(): void {
+    console.log(this.netlifyIdentity)
+    console.log(this.netlifyIdentity.currentUser())
+    // this.netlifyIdentity.init({container: '.netlify-container'})
+    console.log(this.netlifyIdentity.init)
+  }
 }
